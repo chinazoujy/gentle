586f84b261fbb04f6812ae5e26b6a893330d37d2
Tom <chinazoujy@gmail.com>
2017-12-07 18:56:56 +0800

    Update align.py

--- align.py
+++ align.py
@@ -43,12 +43,60 @@ def on_progress(p):
         logging.debug("%s: %s" % (k, v))
 
 
-with open(args.txtfile) as fh:
+with codecs.open(args.txtfile, "r",encoding='utf-8', errors='ignore') as fh:
     transcript = fh.read()
+    print transcript
 
 resources = gentle.Resources()
 logging.info("converting audio to 8K sampled wav")
 
+sens_end_index = None
+with gentle.resampled(args.audiofile) as wavfile:
+    logging.info("starting alignment")
+
+    aligner = gentle.ForcedAligner(resources, transcript, nthreads=args.nthreads, disfluency=args.disfluency, conservative=args.conservative, disfluencies=disfluencies)
+    result = aligner.transcribe(wavfile, progress_cb=on_progress, logging=logging)
+    sens_end_index=aligner.ms.get_sentences_index()
+
+print result
+res = result.to_json()
+res = json.loads(res)
+time_sentences_index = []
+#print len(sens_end_index)
+#print sens_end_index
+
+time_sentences_index = []
+ss_dot = 0
+s_pos = None
+for i,w in enumerate(res['words']):
+    end_v = w['endOffset']
+    start_v = w['startOffset']
+
+    print end_v, sens_end_index[ss_dot]
+    if s_pos is None:
+        s_pos = start_v
+
+    if end_v >= sens_end_index[ss_dot]:
+        ss_dot += 1
+        time_sentences_index.append((s_pos, end_v))
+        if i+1 < len(res['words']):
+            s_pos = res['words'][i+1]['startOffset']
+        else:
+            s_pos = end_v
+
+if len(sens_end_index) != len(time_sentences_index):
+    time_sentences_index.append((s_pos, res['words'][-1]["endOffset"]))
+
+print sens_end_index, len(sens_end_index)
+print time_sentences_index, len(time_sentences_index)
+sens_str=aligner.ms.get_sentences_string()
+for i,t in enumerate(time_sentences_index):
+    print "{{time}}%s/%s{{end}}"%(str(round(float(t[0]),2)),str(round(float(t[1]),2)))
+    print "{{raw}}%s{{end}}"%(sens_str[i])
+
+
+
+'''
 with gentle.resampled(args.audiofile) as wavfile:
     logging.info("starting alignment")
     aligner = gentle.ForcedAligner(resources, transcript, nthreads=args.nthreads, disfluency=args.disfluency, conservative=args.conservative, disfluencies=disfluencies)
@@ -58,3 +106,4 @@ fh = open(args.output, 'w') if args.output else sys.stdout
 fh.write(result.to_json(indent=2))
 if args.output:
     logging.info("output written to %s" % (args.output))
+'''
